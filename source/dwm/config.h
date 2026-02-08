2e12284bad907d0a544084d56a2336dadc4300cd
snoworls <133886718+snowhorls@users.noreply.github.com>
2026-02-08 15:01:35 -0500

    Update color definitions in config.h

--- source/dwm/config.h
+++ source/dwm/config.h
@@ -12,11 +12,12 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#808080";
+static const char col_cyan[]        = "#000000";
+static const char col_borderr[]     = "#808080";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_borderr  },
 };
 
 /* tagging */
